57aabfd5b8093bdf71d7bb7e77b9f8c028d22425
Maid Sabanovic <maid.sabanovic@gws.ms>
2023-08-28 10:56:18 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'corso-website';
+
+  ngOnInit(): void {
+    // Initial check for scroll position
+    this.handleScroll();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event): void {
+    this.handleScroll();
+  }
+
+  private handleScroll(): void {
+    const navbar = document.querySelector('.navbar');
+    if (navbar) {
+      if (window.scrollY > 50) {
+        navbar.classList.add('bg-dark');
+      } else {
+        navbar.classList.remove('bg-dark');
+      }
+    }
+  }
 }
